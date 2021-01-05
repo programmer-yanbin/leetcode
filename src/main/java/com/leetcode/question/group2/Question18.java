3b7ef492b58fc4305c4312212c7eda77f1b5ccd5
yanbin <object_yanbin@163.com>
2021-01-05 23:18:52 +0800

    其他环境提交代码

--- src/main/java/com/leetcode/question/group2/Question18.java
+++ src/main/java/com/leetcode/question/group2/Question18.java
@@ -13,9 +13,9 @@ public class Question18 {
     public static void main(String[] args) {
 
 
-//        System.out.println(System.getProperty("user.home"));
-//        System.out.println(System.getProperty("java.version"));
-//        System.out.println(System.getProperty("os.name"));
-//        System.out.println(System.getProperty("java.vendor.url"));
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("java.vendor.url"));
     }
 }
